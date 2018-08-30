97cc47ccbcf61acbd18b431f72d5aeef27159acc
Aaron Freedman <aaron@secretcrush.net>
2018-08-30 16:52:18 -0400

    Empty example script

---
+++ In Class Demos/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    private GameObject go;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
